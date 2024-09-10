a4dbfbc5705d89b294066ab01c6bc1c7dfc19e19
KwangJin Go <naearu2@gmail.com>
2024-09-10 17:04:44 +0900

    Update deb12_laravel.sh

--- deb12_laravel.sh
+++ deb12_laravel.sh
@@ -7,6 +7,12 @@ apt update && apt -y upgrade
 printf "\n\n## Installation of required items\n\n"
 apt install -y nginx php8.2-fpm php8.2-gd php-json php8.2-mysql php8.2-curl php8.2-mbstring php8.2-intl php-imagick php8.2-xml php8.2-zip php8.2-redis php8.2-bcmath php8.2-soap composer 
 
+echo -ne "\n\n\n\n\n\n\n\n\n" | adduser web
+
+apt install -y nodejs npm sudo vim
+
+sudo usermod -aG sudo web
+
 printf "\n\n## php-fprm \n\n"
 cd /etc/php/8.2/fpm/pool.d/
 sed -i "s/\;slowlog\ \=\ log\/\$pool\.log\.slow/slowlog\ \=\ \/var\/log\/php82-fpm\/www-slow\.log/g" www.conf
@@ -34,16 +40,10 @@ rm /etc/nginx/sites-enabled/default
 wget https://raw.githubusercontent.com/naearu/scripts/main/nginx/laravel.conf -O /etc/nginx/sites-enabled/laravel.conf
  
 
-printf "\n\n## Web Base Dir Setting\n\n"
+printf "\n\n## Dir Setting\n\n"
 
-mkdir -p /home/web/web/public
 mkdir -p /home/web/log/
 mkdir -p /home/web/ssl/
-
-echo "<?php phpinfo(); ?>" > /home/web/web/public/index.php
-chown -R web:web /home/web/web
-
-
 
 ## OPENSSL key
 printf "\n\n## Create SSL key\n\n"
@@ -57,11 +57,13 @@ openssl genrsa -out server.key 2048
 echo -ne "\n\n\n\n\n\n\n\n\n" | openssl req -new -key server.key -out server.csr
 openssl x509 -req -in server.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out server.crt
 
-service nginx restart
-service php8.2-fpm restart
+su web -c "cd ~&&composer create-project laravel/laravel:^10 app"
+su web -c "cd ~/app&&npm install&&npm run build"
+su web -c "cd ~/app&&php artisan storage:link"
 
 chown -R web:web /home/web
 
-su web -c "composer create-project laravel/laravel:^10 app"
+service nginx restart
+service php8.2-fpm restart
 
 
