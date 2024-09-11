5d511cba879d3e02645666849b4f04c7622389d0
KwangJin Go <naearu2@gmail.com>
2024-09-11 11:50:46 +0900

    Update deb12_laravel.sh

--- deb12_laravel.sh
+++ deb12_laravel.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 echo "##################################################"
+
+USERNAME=web
+
+read -p "Enter the new username[default=$USERNAME]: " NEW_USERNAME
+if [[ $NEW_USERNAME != "" ]]; then
+	USERNAME=$NEW_USERNAME
+fi
+
+
+
+
 printf "\n\n## Update & Upgrade \n\n"
 apt update && apt -y upgrade
 
 printf "\n\n## Installation of required items\n\n"
-apt install -y nginx php8.2-fpm php8.2-gd php-json php8.2-mysql php8.2-curl php8.2-mbstring php8.2-intl php-imagick php8.2-xml php8.2-zip php8.2-redis php8.2-bcmath php8.2-soap composer 
+apt install -y nginx php8.2-fpm php8.2-gd php-json php8.2-mysql php8.2-curl php8.2-mbstring php8.2-intl php-imagick php8.2-xml php8.2-zip php8.2-redis php8.2-bcmath php8.2-soap composer nodejs npm sudo vim
 
-echo -ne "\n\n\n\n\n\n\n\n\n" | adduser web
 
-apt install -y nodejs npm sudo vim
+echo -ne "\n\n\n\n\n\n\n\n\n" | adduser $USERNAME
 
-sudo usermod -aG sudo web
+
+sudo usermod -aG sudo $USERNAME
 
 printf "\n\n## php-fprm \n\n"
 cd /etc/php/8.2/fpm/pool.d/
@@ -19,8 +30,8 @@ sed -i "s/\;slowlog\ \=\ log\/\$pool\.log\.slow/slowlog\ \=\ \/var\/log\/php82-f
 sed -i 's/\;request\_slowlog\_timeout\ \=\ 0/request\_slowlog\_timeout\ \=\ 10s/g' www.conf
 sed -i 's/\;request\_terminate\_timeout\ \=\ 0/request\_terminate\_timeout\ \=\ 1200/g' www.conf
 sed -i 's/\;catch\_workers\_output\ \=\ yes/catch\_workers\_output = yes/g' www.conf
-sed -i 's/user\ \=\ www-data/user\ \=\ web/g' www.conf
-sed -i 's/group\ \=\ www-data/group\ \=\ web/g' www.conf
+sed -i 's/user\ \=\ www-data/user\ \=\ $USERNAME/g' www.conf
+sed -i 's/group\ \=\ www-data/group\ \=\ $USERNAME/g' www.conf
 
 mkdir /var/log/php82-fpm
 service php8.2-fpm start
@@ -34,21 +45,23 @@ sed -i 's/memory\_limit\ \=\ 128M/memory\_limit\ \=\ 512M/g' php.ini
 
 printf "\n\n## nginx \n\n"
 cd /etc/nginx/
-sed -i 's/user\ www\-data\;/user\ web\;/g' nginx.conf
+sed -i 's/user\ www\-data\;/user\ $USERNAME\;/g' nginx.conf
 
 rm /etc/nginx/sites-enabled/default
 wget https://raw.githubusercontent.com/naearu/scripts/main/nginx/laravel.conf -O /etc/nginx/sites-enabled/laravel.conf
- 
+
+sed -i 's/\/web\//\/$USERNAME\//g' /etc/nginx/sites-enabled/laravel.conf
+
 
 printf "\n\n## Dir Setting\n\n"
 
-mkdir -p /home/web/log/
-mkdir -p /home/web/ssl/
+mkdir -p /home/$USERNAME/log/
+mkdir -p /home/$USERNAME/ssl/
 
 ## OPENSSL key
 printf "\n\n## Create SSL key\n\n"
 
-cd /home/web/ssl/
+cd /home/$USERNAME/ssl/
 
 openssl genrsa -out rootCA.key 2048
 echo -ne "\n\n\n\n\n\n\n\n\n" | openssl req -new -key rootCA.key -out rootCA.csr
@@ -57,11 +70,11 @@ openssl genrsa -out server.key 2048
 echo -ne "\n\n\n\n\n\n\n\n\n" | openssl req -new -key server.key -out server.csr
 openssl x509 -req -in server.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out server.crt
 
-su web -c "cd ~&&composer create-project laravel/laravel:^10 app"
-su web -c "cd ~/app&&npm install&&npm run build"
-su web -c "cd ~/app&&php artisan storage:link"
+su $USERNAME -c "cd ~&&composer create-project laravel/laravel:^10 app"
+su $USERNAME -c "cd ~/app&&npm install&&npm run build"
+su $USERNAME -c "cd ~/app&&php artisan storage:link"
 
-chown -R web:web /home/web
+chown -R $USERNAME:$USERNAME /home/$USERNAME
 
 service nginx restart
 service php8.2-fpm restart
