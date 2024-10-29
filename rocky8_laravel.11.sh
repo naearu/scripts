3a92b8c6ee1cb277cdec4b1f4b1145285f02da9d
KwangJin Go <naearu2@gmail.com>
2024-10-29 14:25:29 +0900

    Update rocky8_laravel.11.sh

--- rocky8_laravel.11.sh
+++ rocky8_laravel.11.sh
@@ -23,6 +23,7 @@ dnf -y module install nodejs:20/common php:remi-8.2
 yum install -y php php-common php-fpm php-intl \
 php-gd php-json php-curl php-mbstring php-xml php-bcmath \
 php-zip php-soap php-redis php-imagick php-zip \
+php-pgsql php-mysql \
 nginx npm vim wget tar composer
 
 
