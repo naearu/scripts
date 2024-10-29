ac2d57b0e686ee0e975f1dc035406d30fdf43607
KwangJin Go <naearu2@gmail.com>
2024-10-29 14:25:15 +0900

    Update rocky8_laravel.7.sh

--- rocky8_laravel.7.sh
+++ rocky8_laravel.7.sh
@@ -17,7 +17,10 @@ printf "\n\n## Installation of required items\n\n"
 dnf module -y reset nodejs
 dnf module -y enable nodejs:12
 dnf module -y install nodejs:12/common
-yum install -y nginx php php-common php-fpm php-gd php-json php-curl php-mbstring php-intl  php-xml php-zip  php-bcmath php-soap npm vim php-devel php-pear make wget tar epel-release
+yum install -y nginx php php-common php-fpm php-gd php-json \
+php-curl php-mbstring php-intl  php-xml php-zip  php-bcmath \
+php-soap npm vim php-devel php-pear make wget tar epel-release \
+php-pgsql php-mysql
 
 # php-redis
 wget https://pecl.php.net/get/redis-5.3.7.tgz
